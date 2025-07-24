7dea204d200f67f32a6eb697838a12e3f2b56d33
TSheyd <sheydaevtimur@yandex.ru>
2025-07-24 13:48:40 +0300

    fix!: alembic context

--- projects/vedana/packages/vedana-etl/src/vedana_etl/app.py
+++ projects/vedana/packages/vedana-etl/src/vedana_etl/app.py
@@ -1,12 +1,12 @@
 from datapipe_app import DatapipeAPI
 
-from vedana_etl.config import ds
-from vedana_etl.catalog import init_catalog, default_custom_tables
+from vedana_etl.config import ds, catalog
+from vedana_etl.catalog import compile_catalog, init_catalog, default_custom_tables
 from vedana_etl.pipeline import get_pipeline, default_custom_steps
 
 
 # base app - no extra tables / steps
-catalog = init_catalog(catalog_extra_tables=default_custom_tables)
+catalog = init_catalog(catalog, compile_catalog(default_custom_tables))
 pipeline = get_pipeline(custom_steps=default_custom_steps)
 
 app = DatapipeAPI(ds, catalog, pipeline)
