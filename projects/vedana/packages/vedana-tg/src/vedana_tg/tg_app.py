b1c6b8639df7292e0cbd9057b9bd76e01a2c1e1c
TSheyd <sheydaevtimur@yandex.ru>
2025-08-12 14:49:40 +0300

    revert(temp): try loading DataModel from memgraph in tg

--- projects/vedana/packages/vedana-tg/src/vedana_tg/tg_app.py
+++ projects/vedana/packages/vedana-tg/src/vedana_tg/tg_app.py
@@ -53,17 +53,22 @@ async def main_tg():
     try:
         graph = MemgraphGraph(s.memgraph_uri, s.memgraph_user, s.memgraph_pwd)
 
-        data_model = await DataModel.load_from_graph(graph)
-        if data_model is None:
-            logger.info("No DataModel found in graph – loading from Grist …")
-            data_model = DataModel.load_grist_online(
-                s.grist_data_model_doc_id, grist_server=s.grist_server_url, api_key=s.grist_api_key
-            )
+        data_model = DataModel.load_grist_online(
+            s.grist_data_model_doc_id, grist_server=s.grist_server_url, api_key=s.grist_api_key
+        )
 
-            try:
-                await data_model.update_data_model_node(graph)
-            except Exception as e:
-                logger.warning(f"Unable to cache DataModel in graph: {e}")
+        # Todo: tg has no method to update DataModel in graph, and it is not updated in ETL yet
+        # data_model = await DataModel.load_from_graph(graph)
+        # if data_model is None:
+        #     logger.info("No DataModel found in graph – loading from Grist …")
+        #     data_model = DataModel.load_grist_online(
+        #         s.grist_data_model_doc_id, grist_server=s.grist_server_url, api_key=s.grist_api_key
+        #     )
+        #
+        #     try:
+        #         await data_model.update_data_model_node(graph)
+        #     except Exception as e:
+        #         logger.warning(f"Unable to cache DataModel in graph: {e}")
 
         pipeline = RagPipeline(
             graph=graph,
