c92d8c63003638709f7b1a33df9e09ef07e91288
TSheyd <sheydaevtimur@yandex.ru>
2025-08-14 14:01:08 +0300

    old data model update method

--- projects/vedana/packages/vedana-gradio/src/vedana_gradio/gradio_app.py
+++ projects/vedana/packages/vedana-gradio/src/vedana_gradio/gradio_app.py
@@ -13,6 +13,7 @@ from sentry_sdk.integrations.opentelemetry import SentryPropagator, SentrySpanPr
 from vedana_core.data_model import DataModel
 from vedana_core.db import get_sessionmaker
 from vedana_core.graph import MemgraphGraph
+from vedana_core.importers.fast import DataModelLoader
 from vedana_core.settings import settings as s
 
 from vedana_gradio.gradio_ui import create_gradio_interface
@@ -37,7 +38,7 @@ async def make_jims_app() -> fastapi.FastAPI:
         )
 
         try:
-            await data_model.update_data_model_node(graph)
+            DataModelLoader(data_model, graph).update_data_model_node()
         except Exception as e:
             logger.warning(f"Unable to cache DataModel in graph: {e}")
 
