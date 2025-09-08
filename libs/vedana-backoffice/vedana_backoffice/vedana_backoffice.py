ba0857092a5f0d5074d7dbeaf221f015b142517d
TSheyd <sheydaevtimur@yandex.ru>
2025-09-08 20:04:45 +0400

    add chat page

--- libs/vedana-backoffice/vedana_backoffice/vedana_backoffice.py
+++ libs/vedana-backoffice/vedana_backoffice/vedana_backoffice.py
@@ -1,7 +1,7 @@
 import reflex as rx
 
-from vedana_backoffice.pages import etl
-from vedana_backoffice.pages import eval as eval_page
+from vedana_backoffice.pages.chat import page as chat_page
+from vedana_backoffice.pages.etl import page as etl_page
 from vedana_backoffice.pages.jims_thread_list_page import jims_thread_list_page
 from vedana_backoffice.pages.jims_thread_page import jims_thread_page
 from vedana_backoffice.state import EtlState
@@ -17,7 +17,7 @@ def index() -> rx.Component:
                 rx.text("Admin console for ETL, Evaluation, JIMS, and Chatbot"),
                 rx.hstack(
                     rx.link("ETL", href="/etl"),
-                    rx.link("Evaluation", href="/eval"),
+                    rx.link("Chat", href="/chat"),
                     rx.link("JIMS", href="/jims"),
                     spacing="4",
                 ),
@@ -34,8 +34,7 @@ def index() -> rx.Component:
 
 app = rx.App()
 app.add_page(index, route="/", title="Vedana Backoffice")
-app.add_page(etl.page, route="/etl", title="ETL", on_load=EtlState.load_pipeline_metadata)
-app.add_page(eval_page.page, route="/eval", title="Evaluation")
+app.add_page(etl_page, route="/etl", title="ETL", on_load=EtlState.load_pipeline_metadata)
+app.add_page(chat_page, route="/chat", title="Chat")
 app.add_page(jims_thread_list_page, route="/jims", title="JIMS")
 app.add_page(jims_thread_page)
-# app.add_page(chat.page, route="/chat", title="Chatbot")
