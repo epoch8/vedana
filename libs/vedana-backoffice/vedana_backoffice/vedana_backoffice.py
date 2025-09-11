f1ed5964072816b7a809ddc49b7b7acb5dd7ebb7
TSheyd <sheydaevtimur@yandex.ru>
2025-09-11 15:17:38 +0400

    fix dialog handling

--- libs/vedana-backoffice/vedana_backoffice/vedana_backoffice.py
+++ libs/vedana-backoffice/vedana_backoffice/vedana_backoffice.py
@@ -4,7 +4,7 @@ from vedana_backoffice.pages.chat import page as chat_page
 from vedana_backoffice.pages.etl import page as etl_page
 from vedana_backoffice.pages.jims_thread_list_page import jims_thread_list_page
 from vedana_backoffice.pages.jims_thread_page import jims_thread_page
-from vedana_backoffice.state import EtlState
+from vedana_backoffice.state import EtlState, ChatState
 from vedana_backoffice.ui import app_header
 
 
@@ -35,6 +35,6 @@ def index() -> rx.Component:
 app = rx.App()
 app.add_page(index, route="/", title="Vedana Backoffice")
 app.add_page(etl_page, route="/etl", title="ETL", on_load=EtlState.load_pipeline_metadata)
-app.add_page(chat_page, route="/chat", title="Chat")
+app.add_page(chat_page, route="/chat", title="Chat", on_load=ChatState.reset_session)
 app.add_page(jims_thread_list_page, route="/jims", title="JIMS")
 app.add_page(jims_thread_page)
