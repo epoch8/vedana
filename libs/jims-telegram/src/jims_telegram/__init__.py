7f9e8c1f7325d3a3e796eac116721ace92d8777f
TSheyd <sheydaevtimur@yandex.ru>
2025-10-21 23:37:13 +0900

    make session_id reset for telegram on /start

--- libs/jims-telegram/src/jims_telegram/__init__.py
+++ libs/jims-telegram/src/jims_telegram/__init__.py
@@ -1,16 +1,19 @@
 import asyncio
 import uuid
 from contextlib import suppress
-from datetime import datetime
 from typing import Any, Awaitable, overload
+import sqlalchemy as sa
+import sqlalchemy.ext.asyncio as sa_aio
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from jims_core.app import JimsApp
+from jims_core.db import ThreadDB
 from jims_core.schema import Pipeline
 from jims_core.thread.thread_context import StatusUpdater
 from jims_core.thread.thread_controller import ThreadController
+from jims_core.util import uuid7
 from loguru import logger
 from opentelemetry import trace
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -87,6 +90,29 @@ class TelegramController:
             app = await app
         return cls(app)
 
+    @classmethod
+    async def thread_from_user_id(
+        cls,
+        sessionmaker: sa_aio.async_sessionmaker[sa_aio.AsyncSession],
+        from_user_id: int,
+    ) -> "ThreadController | None":
+        """
+        Retrieve the latest thread associated with a given telegram user_id.
+        """
+        async with sessionmaker() as session:
+            stmt = (
+                sa.select(ThreadDB)
+                .where(ThreadDB.thread_config["telegram_user_id"].astext == str(from_user_id))
+                .order_by(ThreadDB.thread_id.desc())  # or created_at(?)
+                .limit(1)
+            )
+            thread = (await session.execute(stmt)).scalar_one_or_none()
+
+        if thread:
+            return ThreadController(sessionmaker, thread)
+        else:
+            return None
+
     async def _run_pipeline(self, ctl: ThreadController, chat_id: Any, pipeline: Pipeline) -> None:
         ctx = await ctl.make_context()
         ctx = ctx.with_status_updater(TelegramStatusUpdater(self.bot, chat_id))
@@ -120,10 +146,10 @@ class TelegramController:
         logger.debug(f"Received command start from {message.chat.id}")
 
         ctl = await self.app.new_thread(
-            uuid_from_int(message.chat.id),
+            # uuid_from_int(message.chat.id),  # makes session_id persist for from_user.id
+            uuid7(),
             {
                 "interface": "telegram",
-                "created_at": str(datetime.now()),
                 "telegram_chat_id": message.chat.id,
                 "telegram_user_id": message.from_user.id if message.from_user else None,
             },
@@ -141,19 +167,19 @@ class TelegramController:
     async def handle_message(self, message: Message) -> None:
         with tracer.start_as_current_span("jims_telegram.handle_message"):
             logger.debug(f"Received message {message.text=} from {message.chat.id=}")
-            ctl = await ThreadController.from_thread_id(
+            ctl = await self.thread_from_user_id(
                 self.app.sessionmaker,
-                uuid_from_int(message.chat.id),
+                message.from_user.id,
             )
 
             if ctl is None:
                 logger.warning(f"Thread with id {message.chat.id} not found, recreating")
                 ctl = await ThreadController.new_thread(
                     self.app.sessionmaker,
-                    uuid_from_int(message.chat.id),
+                    # uuid_from_int(message.chat.id),  # makes session_id persist for from_user.id
+                    uuid7(),
                     {
                         "interface": "telegram",
-                        "created_at": str(datetime.now()),
                         "telegram_chat_id": message.chat.id,
                         "telegram_user_id": message.from_user.id if message.from_user else None,
                     },
