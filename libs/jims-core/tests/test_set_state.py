320d7ec3da8aa856d51c74dfe98c3ada6fa6507c
TSheyd <sheydaevtimur@yandex.ru>
2025-10-30 22:59:10 +0700

    test PipelineState

--- libs/jims-core/tests/test_set_state.py
+++ libs/jims-core/tests/test_set_state.py
@@ -1,13 +1,12 @@
 import pytest
 import pytest_asyncio
 import sqlalchemy.ext.asyncio as sa_aio
+from typing import cast
 from jims_core.db import Base
-from jims_core.thread.thread_context import ThreadContext
-from jims_core.thread.thread_controller import ThreadController
-from jims_core.thread.schema import PipelineState
 from jims_core.schema import Orchestrator
+from jims_core.thread.schema import PipelineState
+from jims_core.thread.thread_controller import ThreadController
 from jims_core.util import uuid7
-from pydantic import BaseModel
 
 
 @pytest_asyncio.fixture(scope="session")
@@ -30,38 +29,28 @@ async def sessionmaker() -> sa_aio.async_sessionmaker[sa_aio.AsyncSession]:
 
 
 @pytest.mark.asyncio
-async def test_set_state(sessionmaker: sa_aio.async_sessionmaker[sa_aio.AsyncSession]) -> None:
+async def test_pipeline_state_write_and_load(
+    sessionmaker: sa_aio.async_sessionmaker[sa_aio.AsyncSession],
+) -> None:
     thread_id = uuid7()
     contact_id = f"test:{thread_id}"
     ctl = await ThreadController.new_thread(sessionmaker, contact_id, thread_id, {})
 
-    class State(BaseModel):
-        key: str
-
-    class FuncOrchestrator(Orchestrator[PipelineState]):
-        def __init__(self, func):
-            self.state = PipelineState()
-            self._func = func
-
+    class DummyOrchestrator(Orchestrator[PipelineState]):
         def route(self, ctx):
-            return self._func
+            async def pipeline(ctx):
+                return None
+            return pipeline
 
-        async def orchestrate(self, ctx):
-            await self._func(ctx)
+        async def orchestrate(self, ctx) -> None:
+            ctx.state.current_pipeline = "alternate"  # change pipeline state
 
-    async def expect_no_state(ctx: ThreadContext) -> None:
-        state = ctx.get_state(State)
-        assert state is None, "Expected no state to be set"
+    ctx_1 = await ctl.make_context()
+    initial = cast(PipelineState, ctx_1.get_or_create_pipeline_state(PipelineState))
+    assert initial.current_pipeline == "main"  # default state
 
-    await ctl.run_with_context(FuncOrchestrator(expect_no_state))
+    await ctl.run_with_context(DummyOrchestrator())
 
-    async def set_state(ctx: ThreadContext) -> None:
-        ctx.set_state(State(key="value"))
-
-    await ctl.run_with_context(FuncOrchestrator(set_state))
-
-    async def get_state(ctx: ThreadContext) -> None:
-        state = ctx.get_state(State)
-        assert state == State(key="value")
-
-    await ctl.run_with_context(FuncOrchestrator(get_state))
+    ctx_2 = await ctl.make_context()
+    loaded = cast(PipelineState, ctx_2.get_or_create_pipeline_state(PipelineState))
+    assert loaded.current_pipeline == "alternate"  # changed during run above
