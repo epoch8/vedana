45705afdc9c59b352727eef66c50a63361a50686
TSheyd <sheydaevtimur@yandex.ru>
2026-01-29 18:43:35 +0700

    revert envs

--- libs/vedana-core/src/vedana_core/db.py
+++ libs/vedana-core/src/vedana_core/db.py
@@ -10,7 +10,7 @@ class DbSettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="", env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
     config_plane_db_conn_uri: str = "postgresql://postgres:postgres@localhost:5432"
-    db_conn_uri: str = "postgresql://postgres:postgres@localhost:5432"
+    jims_db_conn_uri: str = "postgresql://postgres:postgres@localhost:5432"
 
 
 db_settings = DbSettings()  # type: ignore
