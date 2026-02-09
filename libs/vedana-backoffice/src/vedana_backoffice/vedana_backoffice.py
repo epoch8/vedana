45a4ed91c1dfe7476b284b36842d0536a1c57990
TSheyd <sheydaevtimur@yandex.ru>
2026-02-09 14:45:06 +0700

    fix: rm jims page redefinition

--- libs/vedana-backoffice/src/vedana_backoffice/vedana_backoffice.py
+++ libs/vedana-backoffice/src/vedana_backoffice/vedana_backoffice.py
@@ -3,7 +3,7 @@ import reflex as rx
 from vedana_backoffice.pages.chat import page as chat_page
 from vedana_backoffice.pages.etl import page as etl_page
 from vedana_backoffice.pages.eval import page as eval_page
-from vedana_backoffice.pages.jims_thread_list_page import jims_thread_list_page
+from vedana_backoffice.pages.jims_thread_list_page import ThreadListState, jims_thread_list_page
 from vedana_backoffice.pages.main_dashboard import page as main_dashboard_page
 from vedana_backoffice.states.chat import ChatState
 from vedana_backoffice.states.etl import EtlState
@@ -14,5 +14,5 @@ app = rx.App(stylesheets=["/styles.css"])
 app.add_page(main_dashboard_page, route="/", title="Vedana Backoffice", on_load=DashboardState.load_dashboard)
 app.add_page(etl_page, route="/etl", title="ETL", on_load=EtlState.load_pipeline_metadata)
 app.add_page(chat_page, route="/chat", title="Chat", on_load=ChatState.reset_session)
-app.add_page(jims_thread_list_page, route="/jims", title="JIMS")
+app.add_page(jims_thread_list_page, route="/jims", title="JIMS", on_load=ThreadListState.get_data)
 app.add_page(eval_page, route="/eval", title="Evaluation", on_load=EvalState.load_eval_data)
